d90f8db380b9986d78809f4e6f5aa441f7e82f91
Impesud <info@impesud.it>
2023-04-25 18:08:52 +0200

    Update Hero.tsx

--- src/components/Sections/Hero.tsx
+++ src/components/Sections/Hero.tsx
@@ -11,7 +11,7 @@ const Hero: FC = memo(() => {
   const {imageSrc, name, description, actions} = heroData;
 
   return (
-    <Section noPadding sectionId={SectionId.Hero}>
+    <Section noPadding sectionId={SectionId.Hero} className="overflow-hidden">
       <div className="relative flex h-screen w-screen items-center justify-center">
         <Image
           alt={`${name}-image`}
