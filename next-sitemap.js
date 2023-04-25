6483ffeac8cdbaca68dc2a50e75e9bc4060ef139
Impesud <info@impesud.it>
2023-04-25 20:16:11 +0200

    Update various files

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,6 +1,6 @@
 /* eslint-env node */
 module.exports = {
-  siteUrl: 'reactresume.com',
+  siteUrl: 'react-resume-template-impesud.vercel.app',
   exclude: ['/404*', '/500*'],
   transform: async (config, path) => {
     return {
