af86b4ea8fd0a4357af9a90647f080c9ecc1ebda
Impesud <info@impesud.it>
2023-04-26 10:10:38 +0200

    Update Page.tsx

--- src/components/Layout/Page.tsx
+++ src/components/Layout/Page.tsx
@@ -18,8 +18,8 @@ const Page: NextPage<HomepageMeta> = memo(({children, title, description}) => {
         <link href={`https://react-resume-template-impesud.vercel.app${pathname}`} key="canonical" rel="canonical" />
 
         <link href="/apple-touch-icon.png" rel="apple-touch-icon" sizes="180x180" />
-        <link href="/favicon-32x32.png" rel="icon" type="image/png" sizes="32x32" />
-        <link href="/favicon-16x16.png" rel="icon" type="image/png" sizes="16x16" />
+        <link href="/favicon-32x32.png" rel="icon" sizes="32x32" type="image/png" />
+        <link href="/favicon-16x16.png" rel="icon" sizes="16x16" type="image/png" />
         <link color="#5bbad5" href="/safari-pinned-tab.svg" rel="mask-icon" />
         <link href="/site.webmanifest" rel="manifest" />
 
